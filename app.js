c625c1831b95af05600ec383c16c40c67bffd76b
Ahmed Tayyib Kaya <atkahmed9924@gmail.com>
2022-08-14 14:30:57 +0300

    navigation changes

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require("express");
 
 const app = express();
 
+//template engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("/");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`Connect on ${port} port`);
 });
